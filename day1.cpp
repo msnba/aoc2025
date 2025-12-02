17e75cb7f32eed9132e84e1fbe76fb84cecf8dd3
msnba <alex.n.coviello@gmail.com>
2025-12-01 20:59:33 -0600

    added day 1

---
+++ day1.cpp
@@ -0,0 +1,82 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int a(int part);
+
+int main()
+{
+    std::cout << a(1) << std::endl;
+    std::cout << a(2) << std::endl;
+
+    return 0;
+}
+int a(int part)
+{
+    int count = 0;
+    int index = 50;
+    int current = 0;
+    char turn;
+
+    std::ifstream input("day1input.txt");
+    if (!input.is_open())
+        return 1;
+    if (part == 1)
+    {
+        while (input >> turn >> current)
+        {
+            if (turn == 'R')
+            {
+                index = (index + current) % 100;
+            }
+            else
+            {
+                index = (index - current) % 100;
+                if (index < 0)
+                    index += 100;
+            }
+            if (index == 0)
+                count++;
+        }
+    }
+    else
+    {
+        while (input >> turn >> current)
+        {
+            if (turn == 'R')
+            {
+                int a = index + current;
+                count += a / 100;
+                index = a % 100;
+            }
+            else
+            {
+                if (index == 0)
+                {
+                    count += current / 100;
+                }
+                else if (current < index)
+                {
+                }
+                else
+                {
+                    count += (current - index) / 100 + 1;
+                }
+                int a = (index - current) % 100;
+                if (a < 0)
+                    a += 100;
+                index = a;
+            }
+        }
+    }
+
+    return count;
+}
