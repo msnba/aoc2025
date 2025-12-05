181c191b67b1f5aba1224d06a35dd2ef8432166b
msnba <alex.n.coviello@gmail.com>
2025-12-04 23:32:30 -0600

    day 5

---
+++ day5.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <cmath>
+#include "lib.h"
+using namespace std;
+
+Lib l = Lib("day5input.txt");
+
+void p1(Lib &l)
+{
+    int sum = 0;
+    string line;
+    vector<pair<long, long>> ranges;
+    bool cutoff = false;
+    while (getline(l.input, line))
+    {
+        if (line.empty())
+        {
+            cutoff = true;
+            continue;
+        }
+        if (!cutoff)
+        {
+            size_t pos = line.find('-');
+            long start = stol(line.substr(0, pos));
+            long end = stol(line.substr(pos + 1));
+            ranges.push_back({start, end});
+        }
+        else
+        {
+            long id = stol(line);
+            bool fresh = false;
+            for (auto &r : ranges)
+            {
+                if (id >= r.first && id <= r.second)
+                {
+                    fresh = true;
+                    break;
+                }
+            }
+            if (fresh)
+                sum++;
+        }
+    }
+    cout << sum << endl;
+}
+
+void p2(Lib &l)
+{
+    l.ref();
+    long sum2 = 0;
+    string line;
+    vector<pair<long, long>> ranges;
+    while (getline(l.input, line)) // array capture
+    {
+        if (line.empty())
+        {
+            break;
+        }
+        size_t pos = line.find('-');
+        long start = stol(line.substr(0, pos));
+        long end = stol(line.substr(pos + 1));
+        ranges.push_back({start, end});
+    }
+
+    sort(ranges.begin(), ranges.end());
+
+    vector<pair<long, long>> merged; // pairs largest together so it captures all range
+    for (auto &r : ranges)
+    {
+        if (merged.empty() || r.first > merged.back().second) // if the first value in range is larger than merged back second, that means that the range is overall a higher range
+            merged.push_back(r);
+        else
+            merged.back().second = max(merged.back().second, r.second); // finds the highest numerical value to put into that range
+    }
+
+    for (auto &r : merged)
+    {
+        sum2 += r.second - r.first + 1;
+    }
+    cout << sum2 << endl;
+}
+
+int main()
+{
+    p1(l);
+    p2(l);
+    return 0;
+}
